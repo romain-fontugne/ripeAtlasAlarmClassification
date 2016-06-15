60039aa5c5bb2e449b4e3d5d9c5cd2cc19e2a1d3
romain.fontugne <romain.fontugne@gmail.com>
2016-06-15 16:20:35 +0900

    sync

--- classification/classify.py
+++ classification/classify.py
@@ -1,14 +1,20 @@
 
+from pytz import timezone
 import tools
 from bson import objectid
 import networkx as nx
+from datetime import datetime
 
-def makeGraph(start, end):
+def makeGraph(start=datetime(2015,6,10,tzinfo=timezone("UTC")),
+        end=datetime(2015,6,11,tzinfo=timezone("UTC")), min_deviation=3,
+        min_resp=0.5):
     """Construct a graph from delay change and forwarding alarms.
 
     arguments:
     start -- first timestamp
     end -- last timestamp
+    min_deviation -- minimum deviation for delay change alarms
+    min_resp -- minimum responsibility (absolute value) for forwarding alarms 
 
     return: The constructed graph in form of a networkx graph
     """
@@ -19,17 +25,22 @@ def makeGraph(start, end):
     cursor = collection.find({
         "expId": objectid.ObjectId("56d9b1cbb0ab021cc2102c10"),
         # "timeBin": {"$in": timeBin},
-        "$or": [{"timeBin": {"$gte": start}}, {"timeBin": {"$lte": end}}],
+        "$and": [{"timeBin": {"$gte": start}}, {"timeBin": {"$lte": end}}],
         "deviation": {"$gt": 3}
         })
 
     g = nx.Graph()
     for alarm in cursor:
-        g.add_edge(alarm["ipPair"][0], alarm["ipPair"][1],{"dev":alarm["deviation"], "diff":alarm["diff"]})
+        g.add_edge(alarm["ipPair"][0], alarm["ipPair"][1],
+                {   "dev":alarm["deviation"], 
+                    "diff":alarm["diff"],
+                    "time":alarm["timeBin"]
+                })
+        
 
     cursor = collection.find({
-        "expId": objectid.ObjectId(""),
-        "timeBin": timeBin,
+        "expId": objectid.ObjectId("56d9b1cbb0ab021d00224ca8"),
+        "$and": [{"timeBin": {"$gte": start}}, {"timeBin": {"$lte": end}}],
         }) 
     #TODO add forwarding anomalies
 
